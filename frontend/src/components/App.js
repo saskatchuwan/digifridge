1899366a2e7827ff6cd4816cce6c0e381df3c47a
Kathryn <kathryn.chu10@gmail.com>
2019-03-10 10:10:53 -0700

    Abstract away all protected routes and components into separate ProtectedInApp component

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -8,9 +8,7 @@ import Footer from './footer/footer';
 import LoginFormContainer from './session/login_form_container';
 import SignupFormContainer from './session/signup_form_container';
 
-import Fridge from './fridge/fridge';
-import RecipeResultsContainer from './recipe_results/recipe_results_container';
-import SavedRecipesContainer from './saved_recipes/saved_recipes_container';
+import ProtectedInApp from './protectedInApp';
 
 
 class App extends React.Component {
@@ -18,17 +16,13 @@ class App extends React.Component {
     return (
       <div>
         <NavBarContainer />
-        <ProtectedRoute path="/" component={Fridge} />
 
         <Switch>
-          {/* currently, there is no route to / */}
           <AuthRoute exact path="/" component={SplashPage} />
           <AuthRoute exact path="/login" component={LoginFormContainer} />
           <AuthRoute exact path="/signup" component={SignupFormContainer} />
 
-          {/* protected routes: will include /home (recipe results) and /saved (user saved recipes */}
-          <ProtectedRoute path='/home' component={RecipeResultsContainer} />
-          <ProtectedRoute path='/saved' component={SavedRecipesContainer} />
+          <ProtectedRoute path='/' component={ProtectedInApp} />
         </Switch>
         
         {/* optional footer */}
---
+++ frontend/src/components/protectedInApp.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { ProtectedRoute } from '../util/route_util';
+import { Switch } from 'react-router-dom';
+
+import Fridge from './fridge/fridge';
+import RecipeResultsContainer from './recipe_results/recipe_results_container';
+import SavedRecipesContainer from './saved_recipes/saved_recipes_container';
+
+
+class ProtectedInApp extends React.Component {
+  render() {
+    return (
+      <div className='main-in-app-container'>
+        <ProtectedRoute path="/" component={Fridge} />
+        <Switch>
+          <ProtectedRoute path='/home' component={RecipeResultsContainer} />
+          <ProtectedRoute path='/saved' component={SavedRecipesContainer} />
+        </Switch>
+      </div>
+    );
+  }
+}
+
+export default ProtectedInApp;
