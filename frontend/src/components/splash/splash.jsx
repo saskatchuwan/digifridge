345de56f359d39ab05c19b3b1971f181b2c324fc
Kathryn <kathryn.chu10@gmail.com>
2019-03-10 16:29:58 -0700

    Minor code format change

--- frontend/src/components/splash/splash.jsx
+++ frontend/src/components/splash/splash.jsx
@@ -8,7 +8,13 @@ class SplashPage extends React.Component {
       <div className='splash-main'>
       
         <div className='video-hero'>
-          <video poster="https://image-service.blueapron.com/render/q/quality/75/src/https%3A%2F%2Fmedia.blueapron.com%2Fhome_page%2FSplash%2Fbrand-hero-poster.jpg" preload="auto" autoplay="true" loop='true' muted="true" class="vsc-initialized" data-vscid="3rbb7xa52">
+          <video poster="https://image-service.blueapron.com/render/q/quality/75/src/https%3A%2F%2Fmedia.blueapron.com%2Fhome_page%2FSplash%2Fbrand-hero-poster.jpg" 
+                  preload="auto" 
+                  autoplay="true" 
+                  loop='true' 
+                  muted="true" 
+                  class="vsc-initialized" 
+                  data-vscid="3rbb7xa52">
             <source src="https://media.blueapron.com/home_page/Splash/brand-hero.mp4" type="video/mp4" />
           </video>
         </div>
