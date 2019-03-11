c57263c6dae4a3136a536fafd480b99c581f7d6c
marianna mullens <mariannamullens@gmail.com>
2019-03-11 09:32:28 -0700

    Add support for production API keys

---
+++ config/keys_prod.js
@@ -0,0 +1,7 @@
+module.exports = {
+  mongoURI: process.env.MONGO_URI,
+  secretOrKey: process.env.SECRET_OR_KEY,
+  edamamAppId: process.env.EDAMAM_APP_ID,
+  edamamAppSecret: process.env.EDAMAM_APP_SECRET,
+  food2ForkKey: process.env.FOOD2FORK_KEY
+};
