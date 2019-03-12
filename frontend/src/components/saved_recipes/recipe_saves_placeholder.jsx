0cdc2bfbfeb6ad1c60bb8a04b1a74490b48c3478
Kathryn <kathryn.chu10@gmail.com>
2019-03-12 16:12:08 -0700

    Create placeholder for empty recipe results container and empty saved recipes container

---
+++ frontend/src/components/saved_recipes/recipe_saves_placeholder.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+const RecipeSavesPlaceholder = () => {
+  return (
+    <div className='saves placeholder'>
+      <h1>Aww, it looks like you haven't saved any recipes...</h1>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+    </div>
+  );
+}
+
+export default RecipeSavesPlaceholder;
+
