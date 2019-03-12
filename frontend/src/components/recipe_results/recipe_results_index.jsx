856cb79b654d5c678872e512c8ae360b7e214210
marianna mullens <mariannamullens@gmail.com>
2019-03-11 17:40:34 -0700

    Add components for recipes and recipe saves

--- frontend/src/components/recipe_results/recipe_results_index.jsx
+++ frontend/src/components/recipe_results/recipe_results_index.jsx
@@ -7,12 +7,24 @@ class RecipeResultsIndex extends React.Component {
     super(props);
   }
 
+  componentDidMount() {
+    if (this.props.fetch) this.props.fetchRecipeSaves();
+  }
+
   render() {
+    let { recipes, handleRecipe, buttonText } = this.props;
+    let recipeItems = recipes.map( recipe => (
+      <RecipeResultsIndexItem 
+        key={recipe.linkUrl}
+        recipe={recipe}
+        handleRecipe={handleRecipe}
+        buttonText={buttonText}
+      />
+    ));
 
     return (
       <div id='recipe-results' className='recipes-main-container'>
-        {/* need to create list of fridge index items */}
-        <RecipeResultsIndexItem />
+        {recipeItems}
       </div>
     );
   }
